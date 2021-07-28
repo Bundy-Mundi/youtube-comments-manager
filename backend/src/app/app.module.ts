240e4a72d499e34828a6491c71e001f8d38f0378
Bundy-Mundi <bennie.ben095@gmail.com>
2021-07-28 21:54:42 +0900

    Changed filestructure

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CommentsModule } from './comments/comments.module';
-import { AuthModule } from './auth/auth.module';
+import { CommentsModule } from '../comments/comments.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [CommentsModule, AuthModule],
