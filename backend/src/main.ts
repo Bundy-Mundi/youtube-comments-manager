1e9317ac3785194def5c2f22bbb454956e749bb9
Bundy-Mundi <bennie.ben095@gmail.com>
2021-07-31 14:01:40 +0900

    Add cookie-parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ApiRouterModule } from './api.router';
+import * as cookieParser from 'cookie-parser';
+
 //import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiRouterModule);
   app.enableCors();
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
