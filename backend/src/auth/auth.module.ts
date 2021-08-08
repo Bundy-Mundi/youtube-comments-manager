f2801f91f7703ec50cc68c748a89b8aed65908be
Bundy-Mundi <bennie.ben095@gmail.com>
2021-08-08 13:52:09 -0700

    Add RedisModule to AuthModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { RedisModule } from 'src/redis/redis.module';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './auth.jwt.strategy';
 import { YouTubeStrategy } from './auth.youtube';
 require("dotenv").config();
 
 @Module({
-    imports:[PassportModule, JwtModule.register({
+    imports:
+    [
+      PassportModule,
+      RedisModule,
+      JwtModule.register({
         secret: process.env.JWT_SECRET
-      })],
+      })
+    ],
     providers:[YouTubeStrategy, JwtStrategy],
     controllers:[AuthController]
 })
